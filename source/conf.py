d27b00533e14a7e3e8e8434344af5478d4bdd16d
Côme ALLART <numero_744@net-c.com>
2022-10-25 12:53:40 +0200

    set author to 'SpinalHDL contributors'

--- source/conf.py
+++ source/conf.py
@@ -21,7 +21,7 @@
 
 project = 'SpinalHDL'
 copyright = '2022, SpinalHDL'
-author = 'Spinal'
+author = 'SpinalHDL contributors'
 
 # The short X.Y version
 version = ''
@@ -135,7 +135,7 @@ latex_elements = {
 #  author, documentclass [howto, manual, or own class]).
 latex_documents = [
     (master_doc, 'SpinalHDL.tex', 'SpinalHDL Documentation',
-     'penis', 'manual'),
+     author, 'manual'),
 ]
 
 
