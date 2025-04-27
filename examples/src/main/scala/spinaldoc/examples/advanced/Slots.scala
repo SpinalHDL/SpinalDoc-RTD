1a8ef82310604e295b2410ff7308e2c299994998
Marc Emery <marc.emery@amnis.ch>
2025-04-27 08:54:18 +0200

    Improve presentation in slot doc

--- examples/src/main/scala/spinaldoc/examples/advanced/Slots.scala
+++ examples/src/main/scala/spinaldoc/examples/advanced/Slots.scala
@@ -8,36 +8,38 @@ case class SlotsDemo(slotsCount : Int) extends Component {
   // ...
   
   
-  // Create the hardware for each slot
-  // Note each slot is an Area, not a Bundle
+  // Create the hardware for each slot.
+  // Note each slot is an Area, not a Bundle.
   val slots = for(i <- 0 until slotsCount) yield new Area {
-    // Because the slot is an Area, we can define mix signal, registers, logic definitions
-    // Here are the registers for each slots
+    // Because the slot is an Area, we can define mix signals, registers, 
+    // logic definitions.
+    // Here are the registers for each slots:
     val valid = RegInit(False)
     val address = Reg(UInt(8 bits))
     val age = Reg(UInt(16 bits)) // Will count since how many cycles the slot is valid
 
-    // Here is some hardware behavior for each slots
-    // Implement the age logic
+    // Here is some hardware behavior for each slots.
+    // Implement the age logic.
     when(valid) {
       age := age + 1
     }
 
-    // removeIt will be used as a slot interface later on
+    // removeIt will be used as a slot interface later on.
     val removeIt = False
     when(removeIt) {
       valid := False
     }
   }
 
-  // Logic to allocate a new slot
+  // Logic to allocate a new slot.
   val insert = new Area {
-    val cmd = Stream(UInt(8 bits)) // interface to issue requests
+    val cmd = Stream(UInt(8 bits)) // Interface to issue requests.
     val free = slots.map(!_.valid)
-    val freeOh = OHMasking.first(free) // Get the first free slot (on hot mask)
-    cmd.ready := free.orR // Only allow cmd when there is a free slot
+    val freeOh = OHMasking.first(free) // Get the first free slot (on hot mask).
+    cmd.ready := free.orR // Only allow cmd when there is a free slot.
     when(cmd.fire) {
-      // slots.onMask(freeOh)(code) will execute the code for each slot where the corresponding freeOh bit is set
+      // slots.onMask(freeOh)(code) will execute the code for each slot where
+      // the corresponding freeOh bit is set
       slots.onMask(freeOh){slot =>
         slot.valid := True
         slot.address := cmd.payload
@@ -46,17 +48,21 @@ case class SlotsDemo(slotsCount : Int) extends Component {
     }
   }
 
-  // Logic to remove the slots which match a given address (assuming there is not more than one match)
+  // Logic to remove the slots which match a given address (assuming
+  // there is not more than one match).
   val remove = new Area {
-    val cmd = Flow(UInt(8 bits)) // interface to issue requests
-    val oh = slots.map(s => s.valid && s.address === cmd.payload) // oh meaning "one hot"
+    val cmd = Flow(UInt(8 bits)) // Interface to issue requests.
+    // oh meaning "one hot"
+    val oh = slots.map(s => s.valid && s.address === cmd.payload)
     when(cmd.fire) {
       slots.onMask(oh){ slot =>
         slot.removeIt := True
       }
     }
 
-    val reader = slots.reader(oh) // Create a facility to read the slots using "oh" as index
+    // Create a facility to read the slots using "oh" as index
+    val reader = slots.reader(oh)
+
     val age = reader(_.age) // Age of the slot which is selected by "oh"
   }
   
